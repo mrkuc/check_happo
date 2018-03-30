af9f3ca97819a4302e1e5fbaf3d15de5357123f4
shiimaxx <yoshima@shiimaxx.com>
2018-03-30 13:55:36 +0900

    Remove responseBody for output message

--- command/monitor.go
+++ command/monitor.go
@@ -77,7 +77,7 @@ func cmdMonitor(verbose bool, pluginName, pluginOption string, proxy []string, h
 		if responseStatusCode == http.StatusGatewayTimeout {
 			return fmt.Sprintf(remoteUnknownMessageFormat, err.Error()), halib.MonitorUnknown
 		}
-		msg := fmt.Sprintf("%s %s", err.Error(), responseBody)
+		msg := fmt.Sprintf("%s", err.Error())
 		return fmt.Sprintf(remoteErrorMessageFormat, msg), halib.MonitorError
 	}
 
